495887badb82c739879a9eb04a0b5690960e2eee
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-05-22 14:24:12 +0100

    Add meta_types management to EPHooks

--- classes/ElasticPressHooks.php
+++ classes/ElasticPressHooks.php
@@ -6,27 +6,56 @@ class ElasticPressHooks
 {
     public function __construct()
     {
-        $this->actions();
+        $this->hooks();
     }
 
     /**
      * Registers WP actions
      * This method is to be initialised on construct
      */
-    public function actions()
+    public function hooks()
     {
-        add_filter('ep_elasticsearch_plugins', [$this, 'elasticsearchPlugins']);
+        add_filter('ep_elasticsearch_plugins', [$this, 'filterPlugins']);
+        add_filter('ep_allowed_documents_ingest_mime_types', [$this, 'filterMimeTypes']);
     }
 
     /**
-     * Add non-listed ES plugins to filtered array
+     * Add non-listed AWS ES plugins to filtered array
      * @param array|bool $es_plugins
      * @return array
      */
-    public function elasticsearchPlugins($es_plugins): array
+    public function filterPlugins($es_plugins): array
     {
-        $es_plugins['ingest-attachment'] = true;
+        $es_plugins['ingest-attachment'] = sanitize_text_field(EP_VERSION) ?? true;
 
         return $es_plugins;
+    }
+
+    /**
+     * Add or remove document formats. Filtering here will affect attachment indexing.
+     * ES uses Apache Tika to convert binary documents into text and meta data.
+     * For a full list of available formats, use the link below.
+     *
+     * @param $mime_types = [
+     *           'pdf' => 'application/pdf',
+     *           'ppt' => 'application/vnd.ms-powerpoint',
+     *           'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+     *           'xls' => 'application/vnd.ms-excel',
+     *           'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+     *           'doc' => 'application/msword',
+     *           'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+     *       ];
+     * @return array
+     * @link https://tika.apache.org/1.24.1/formats.html#Full_list_of_Supported_Formats
+     */
+    public function filterMimeTypes($mime_types): array
+    {
+        // add the open document spreadsheet format
+        $mime_types['ods'] = 'application/vnd.oasis.opendocument.spreadsheet';
+
+        // remove a mime-type
+        unset($mime_types['ods']);
+
+        return $mime_types;
     }
 }
