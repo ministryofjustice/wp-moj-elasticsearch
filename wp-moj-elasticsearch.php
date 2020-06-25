fb71f8b25a1102062e8eff8f53de3b4cff41d9c9
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2020-06-25 20:16:43 +0100

    Version bump

--- wp-moj-elasticsearch.php
+++ wp-moj-elasticsearch.php
@@ -3,7 +3,7 @@
  * Plugin name: WP MoJ ElasticSearch
  * Plugin URI:  https://github.com/ministryofjustice/wp-moj-elasticsearch
  * Description: MoJ WP plugin to extend the functionality of the ElasticPress plugin
- * Version:     2.1.0
+ * Version:     2.1.1
  * Authors:     Damien Wilson, Adam Brown
  * Text domain: wp-moj-elasticsearch
  * Author URI:  https://ministryofjustice.github.io/justice-on-the-web
