5a391e93816cbfa7225e9c1a0c87e76bc15dee33
adam <adam.brown@digital.justice.gov.uk>
2020-12-04 16:56:45 +0000

    Store DB meta in transient to reduce SQL calls

--- classes/ElasticPressHooks.php
+++ classes/ElasticPressHooks.php
@@ -269,7 +269,15 @@ class ElasticPressHooks
         OR meta_key like 'built_in%'
         OR meta_key like 'choice_%'";
 
-        $meta_keys = $wpdb->get_col($wpdb->prepare($query));
+        // Store DB query in a transient to reduce SQL calls slowing indexing
+        if (false === ($meta_keys = get_transient( 'moj_es_exclude_meta_fields' ))) {
+
+            $meta_keys = $wpdb->get_col($wpdb->prepare($query));
+
+            set_transient( 'moj_es_exclude_meta_fields', $meta_keys, MONTH_IN_SECONDS );
+        }
+
+        $meta_keys = maybe_unserialize($meta_keys);
 
         foreach ($meta_keys as $meta_key){
             $excluded[] = $meta_key;
