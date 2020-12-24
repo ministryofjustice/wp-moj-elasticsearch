daee90b71801ea19e37d1df2f667cdb655f9b1a2
RobLoweMOJ <51912126+RobLoweMOJ@users.noreply.github.com>
2020-12-24 15:40:43 +0000

    Version Bump 2.2.3

--- wp-moj-elasticsearch.php
+++ wp-moj-elasticsearch.php
@@ -3,7 +3,7 @@
  * Plugin name: WP MoJ ElasticSearch
  * Plugin URI:  https://github.com/ministryofjustice/wp-moj-elasticsearch
  * Description: MoJ WP plugin to extend the functionality of the ElasticPress plugin
- * Version:     2.2.2
+ * Version:     2.2.3
  * Authors:     Damien Wilson, Adam Brown, Robert Lowe
  * Text domain: wp-moj-elasticsearch
  * Author URI:  https://ministryofjustice.github.io/justice-on-the-web
