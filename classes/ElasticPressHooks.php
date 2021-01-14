9966d6c0a1677a091e091a189071e6e8e29b4315
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-01-14 11:54:49 +0000

    Add ES Signing

--- classes/ElasticPressHooks.php
+++ classes/ElasticPressHooks.php
@@ -19,9 +19,9 @@ class ElasticPressHooks
      */
     private $alias_name = '';
 
-    public function __construct($alias)
+    public function __construct()
     {
-        $this->alias_name = $alias->name;
+       // $this->alias_name = $alias->name;
         $this->hooks();
     }
 
@@ -31,12 +31,12 @@ class ElasticPressHooks
      */
     public function hooks()
     {
-        add_filter('ep_elasticsearch_plugins', [$this, 'filterPlugins']);
-        add_filter('ep_allowed_documents_ingest_mime_types', [$this, 'filterMimeTypes']);
-        add_filter('ep_index_name', [$this, 'aliasName'], 10, 1);
+      //  add_filter('ep_elasticsearch_plugins', [$this, 'filterPlugins']);
+        //add_filter('ep_allowed_documents_ingest_mime_types', [$this, 'filterMimeTypes']);
+        //add_filter('ep_index_name', [$this, 'aliasName'], 10, 1);
         add_filter('ep_prepare_meta_excluded_public_keys', [$this, 'excludeMetaMappingFields'], 10, 2);
-        add_filter('ep_index_default_per_page', [$this, 'indexPerPage'], 10, 1);
-        add_filter('ep_config_mapping_request', [$this, 'mapRequest'], 10, 1);
+        //add_filter('ep_index_default_per_page', [$this, 'indexPerPage'], 10, 1);
+        //add_filter('ep_config_mapping_request', [$this, 'mapRequest'], 10, 1);
         add_filter('ep_post_sync_args_post_prepare_meta', [$this, 'removePostArgs'], 10, 2);
     }
 
--- wp-moj-elasticsearch.php
+++ wp-moj-elasticsearch.php
@@ -16,87 +16,13 @@
  */
 define('MOJ_ES_DIR', __DIR__);
 
-add_filter('ep_prepare_meta_excluded_public_keys', 'excludeMetaMappingFields', 10, 2);
+require(MOJ_ROOT_DIR . '/vendor/autoload.php');
 
-function excludeMetaMappingFields($keys, $post): array
-{
-    global $wpdb;
+use MOJElasticSearch\ElasticPressHooks;
+use MOJElasticSearch\SignAmazonEsRequests;
 
-    $excluded = [
-        'lbfw_likes',
-        'lhs_menu_on',
-        'dw_comments_on',
-        'oasis_current_revision',
-        'comment_disabled_status',
-        'dw_hide_page_details',
-        'dw_hq_guidance_bottom',
-        'keywords',
-        'related_docs_scanned',
-        'related_docs',
-        'is_imported',
-        'dw_lhs_menu_on',
-        'disable_banner',
-        'dw_banner_link',
-        'dw_banner_url',
-        'dw_campaign_colour',
-        'dw_campaign_skin',
-        'dw_hq_guidance_bottom',
-        'enable_agency_about_us',
-        'dw_tag',
-        'fork_from_post_id',
-        'enable_moj_about_us',
-        'enable_agency_about_us',
-        'full_width_page_banner',
-        'oasis_is_in_workflow',
-        'moj_description',
-        'amazonS3_cache',
-        'amazonS3_info',
-        'guidance_tabs'
-    ];
-
-    $query = "SELECT DISTINCT meta_key from `wp_postmeta`
-        where meta_key like '%_html_content'
-        OR meta_key like '%_links'
-        OR meta_key like '%_sections'
-        OR meta_key like '%_link_url'
-        OR meta_key like '%_link_type'
-        OR meta_key like 'content_section%'
-        OR meta_key like 'built_in%'
-        OR meta_key like 'choice_%'";
-
-    // Store DB query in a transient to reduce SQL calls slowing indexing
-    if (false === ($meta_keys = get_transient('moj_es_exclude_meta_fields'))) {
-        $meta_keys = $wpdb->get_col($wpdb->prepare($query));
-        set_transient('moj_es_exclude_meta_fields', $meta_keys, MONTH_IN_SECONDS);
-    }
-
-    $meta_keys = maybe_unserialize($meta_keys);
-
-    foreach ($meta_keys as $meta_key) {
-        $excluded[] = $meta_key;
-    }
-
-    return $excluded;
-}
-
-add_filter('ep_post_sync_args_post_prepare_meta', 'removePostArgs', 10, 2);
-
-function removePostArgs($post_args, $post_id): array
-{
-    unset($post_args['post_author']);
-    unset($post_args['comment_count']);
-    unset($post_args['post_content_filtered']);
-    unset($post_args['post_parent']);
-    unset($post_args['comment_status']);
-    unset($post_args['ping_status']);
-    unset($post_args['menu_order']);
-    unset($post_args['guid']);
-
-    return $post_args;
-}
-
-
-
+new ElasticPressHooks;
+new SignAmazonEsRequests;
 
 
 /**
