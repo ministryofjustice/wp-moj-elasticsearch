aaae97c9a9e59a1d90b5f62a1a8ca450dd813de6
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-01-15 10:08:03 +0000

    Strip Functionality so we can retro step through the code and reduce complexity

--- wp-moj-elasticsearch.php
+++ wp-moj-elasticsearch.php
@@ -3,7 +3,7 @@
  * Plugin name: WP MoJ ElasticSearch
  * Plugin URI:  https://github.com/ministryofjustice/wp-moj-elasticsearch
  * Description: MoJ WP plugin to extend the functionality of the ElasticPress plugin
- * Version:     2.3.0
+ * Version:     2.3.1
  * Authors:     Damien Wilson, Adam Brown, Robert Lowe
  * Text domain: wp-moj-elasticsearch
  * Author URI:  https://ministryofjustice.github.io/justice-on-the-web
@@ -19,46 +19,9 @@ define('MOJ_ES_DIR', __DIR__);
 require(MOJ_ROOT_DIR . '/vendor/autoload.php');
 
 use MOJElasticSearch\ElasticPressHooks;
+use MOJElasticSearch\Index;
 use MOJElasticSearch\SignAmazonEsRequests;
 
 new ElasticPressHooks;
+new Index;
 new SignAmazonEsRequests;
-
-
-/**
- * Disable ElasticPress dashboard sync
- */
-/*
-define('EP_DASHBOARD_SYNC', false);
-
-// Load all our classes from PSR4 autoloader
-require(MOJ_ROOT_DIR . '/vendor/autoload.php');
-
-use MOJElasticSearch\Options;
-use MOJElasticSearch\Settings\Page;
-use MOJElasticSearch\Alias;
-use MOJElasticSearch\Admin;
-use MOJElasticSearch\Auth;
-use MOJElasticSearch\ElasticPressHooks;
-use MOJElasticSearch\Index;
-use MOJElasticSearch\Query;
-use MOJElasticSearch\ManageData;
-use MOJElasticSearch\SignAmazonEsRequests;
-
-// settings
-use MOJElasticSearch\Settings\IndexSettings;
-
-$moj_es_auth = new Auth();
-if ($moj_es_auth->ok) {
-    $moj_es_admin = new Admin();
-    $moj_es_alias = new Alias($moj_es_admin);
-    new ElasticPressHooks($moj_es_alias);
-    $moj_es_settings = new IndexSettings($moj_es_admin, $moj_es_alias);
-    new Options;
-    new Page;
-    new SignAmazonEsRequests;
-    new ManageData();
-    new Index($moj_es_settings, $moj_es_alias);
-    new Query();
-}
-*/
