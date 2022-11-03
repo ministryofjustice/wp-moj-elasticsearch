9bfb7e185fb3adeafd26ed3ae96d67e22523f2fc
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2022-11-03 16:18:49 +0000

    Add meta data restrictions to ElasticPress index

--- classes/Index.php
+++ classes/Index.php
@@ -35,6 +35,8 @@ class Index
     public function hooks()
     {
         add_filter('ep_index_name', [$this, 'indexNames'], 11, 1);
+        add_filter('ep_prepare_meta_data', [$this, 'limitACFMeta']);
+        add_filter('ep_prepare_meta_excluded_public_keys', [$this, 'excludeMeta']);
     }
 
 
@@ -79,7 +81,7 @@ class Index
         $new_index = $index_names[$new_index_names][$new_index_key]; // string
 
         $namespace = (function_exists('env') ? env('ES_INDEX_NAMESPACE') : null);
-        $new_index =  ($namespace ? $namespace . "." : "") . $new_index;
+        $new_index = ($namespace ? $namespace . "." : "") . $new_index;
 
         $site_url = get_site_url();
 
@@ -96,5 +98,29 @@ class Index
         update_option('_moj_es_current_index_name', $new_index);
 
         return $new_index;
+    }
+
+    public function limitACFMeta($meta): array
+    {
+        foreach ($meta as $key => $value) {
+            if (preg_match('/.+_([0-9]+)_.+/', $key)) {
+                unset($meta[$key]);
+            }
+        }
+
+        return $meta;
+    }
+
+    public function excludeMeta(): array
+    {
+        return [
+            "built_in_post_types_event", "built_in_post_types_task", "built_in_post_types_projects",
+            "built_in_post_types_vacancies", "built_in_post_types_glossaryitem", "built_in_taxonomies_category",
+            "built_in_taxonomies_post_tag", "built_in_taxonomies_link_category", "built_in_taxonomies_grade",
+            "built_in_taxonomies_team", "built_in_taxonomies_document_type", "built_in_taxonomies_atoz",
+            "built_in_taxonomies_agency", "built_in_taxonomies_author", "built_in_taxonomies_dw_snippet",
+            "built_in_taxonomies_snippet_categories", "built_in_taxonomies_workflow_state", "built_in_taxonomies_news_category",
+            "built_in_taxonomies_resource_category", "built_in_post_types_news"
+        ];
     }
 }
